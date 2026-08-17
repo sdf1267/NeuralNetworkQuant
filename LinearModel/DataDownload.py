224fb9aa0efcd165c5c416f552ab7d17153858d1
sdf1267 <56427957+sdf1267@users.noreply.github.com>
2026-08-17 16:03:04 +0200

    Pandas analysis of stock/crypto data

--- LinearModel/DataDownload.py
+++ LinearModel/DataDownload.py
@@ -18,27 +18,17 @@ def download_data(
     for symb in symbol:
 
         data[("close_log_return",symb)] = np.log(data[("Close",symb)]) - np.log(data[("Close",symb)].shift(1))
+        data[("close_cumulative_log_return",symb)] =  data[("close_log_return",symb)].cumsum()
         data[("open_close_log_diff",symb)] = np.log(data[("Open",symb)]) - np.log(data[("Close",symb)].shift(1))
         data[("overnight_log_gap",symb)] = np.log(data[("Open",symb)]) - np.log(data[("Open",symb)].shift(1)) # measured with the day and the previous date
     return data
 
-def append_lag_log_return(  data:pd.DataFrame,  lag:int, symb: str):
+def append_lag_close_log_return(data:pd.DataFrame,  lag:int, symbol: str):
     if lag == 0:
         raise ValueError("lag must be greater than 0")
     if type(lag) != int:
         raise TypeError("lag must int")
 
     name = 'close_log_return_lag_' + str(lag)
-    data[name] = data[("close_log_return", symb)].shift(lag)
+    data[(name,symbol)] = data[("close_log_return", symbol)].shift(lag)
     return data
-
-
-ticker = ['AAPL','MSFT', "^GSPC", "BTC-USD"]
-
-start = "2022-01-01"
-end = "2026-03-31"
-interval = "1d"
-
-data = download_data(ticker, start, end, interval)
-print(data)
-# %%
--- LinearModel/LinearModel.py
+++ LinearModel/LinearModel.py
@@ -16,3 +16,4 @@ class LinearModel(nn.Module):
 
     def forward(self, x):
         return self.linear(x)
+    
